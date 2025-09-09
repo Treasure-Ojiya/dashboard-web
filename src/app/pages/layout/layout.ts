43eee602a1b109f86289a70fcfda2cac8192bd96
Treasure Ojiya <ansgarojiya@gmail.com>
2025-09-09 15:26:27 +0100

    commit for dashboard

--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -1,13 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
 export class Layout {
+  // dropdown states
+  // isReportsOpen = false;
+  // isSettingsOpen = false;
+
+  // // sidebar state
+  // isSidebarOpen = false;
+
+  // toggleReports() {
+  //   this.isReportsOpen = !this.isReportsOpen;
+  // }
+
+  // toggleSettings() {
+  //   this.isSettingsOpen = !this.isSettingsOpen;
+  // }
+
+  // toggleSidebar() {
+  //   this.isSidebarOpen = !this.isSidebarOpen;
+  // }
+
+  // closeSidebar() {
+  //   this.isSidebarOpen = false;
+  // }
+
+  sidebarOpen = false;
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
   router = inject(Router);
   logoutFn() {
     localStorage.removeItem('leaveUser');
